e85e70b031b8a3fbf9f6f720b95cb24f8084ece4
JaRenie-spec <renie.jeanalain@gmail.com>
2025-04-22 15:15:35 +0200

    adding import cors

--- part-4/run.py
+++ part-4/run.py
@@ -3,10 +3,23 @@ from flask_cors import CORS
 
 app = create_app()
 
-CORS(app)
+app.url_map.strict_slashes = False
+
+CORS(
+    app,
+    origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ],
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    expose_headers=["Authorization"]
+)
 
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
